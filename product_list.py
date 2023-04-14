3b111762d7cc65b085f9c18545c8d0de30a32715
RenaldasZ <spikeris2@gmail.com>
2023-04-14 15:48:43 +0300

    pridejau miltu produktus

---
+++ product_list.py
@@ -0,0 +1,14 @@
+
+Miltai = {"kvieciu", "rugiu", "miežiu", "kukuruzu", "ryziu"}
+
+Tešlos =  {"makaronai", "duonos gaminiai", "blynai", "koldūnai"}
+
+Kepiniai = {"pyragai", "tortai", "sausainiai", "misiniai"}
+
+Padažai_prieskoniai = {"miltiniai padažai", "prieskoniai"}
+
+Gerimai = {"alus", "viskis", "romas"}
+
+Pusgaminiai = {"traskuciai", "krekeriai", "duonos trupiniai"}
+
+Kitos_iltinių_produktų_rūšys = {"dribsniai", "grūdėtų miltų produktai", "miltų mišiniai"}
