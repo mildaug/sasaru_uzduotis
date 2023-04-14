47d030fd3bf91720b6211953fe9770ae14672763
Kobrius11 <kobrius11@gmail.com>
2023-04-14 15:21:17 +0300

    updated main.py

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
 
+
+
+while True:
+    print("""
+    1 - kaskokia operacija
+    2 - kaskokia operacija
+    3 - exit, iseiti is ciklo
+    """
+    )
+    try:
+        choice = int(input("pasirinkite norima operacija (1-2-3) :"))
+    except:
+        print("NaN - Not a Number!")
+        continue
+
+
+    if choice == 1:
+        pass
+    
+    elif choice == 2:
+        pass
+    
+
+
+    elif choice == 3:
+        break
+    else:
+        if choice > 3 or choice < 1:
+            print("invalid choice")
+            continue
